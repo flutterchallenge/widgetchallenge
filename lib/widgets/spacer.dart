ef246c64bb98ef7f8d8f40ac461c6c39c10e9622
yubaraj poudel <yubarajpoudel708@gmail.com>
2019-06-05 23:03:07 +0545

    code uncommented

--- lib/widgets/spacer.dart
+++ lib/widgets/spacer.dart
@@ -41,61 +41,61 @@ class AlignmentWithSpacer extends StatelessWidget {
 }
 
 //Allignment example
-//class RowColumnAlignment extends StatelessWidget {
-//  List colors = [Colors.teal, Colors.red, Colors.blue];
-//
-//  _sampleWidget() => List<Widget>.generate(colors.length, (index) {
-//        return new Container(
-//          color: colors[index],
-//          height: 50,
-//          width: 50,
-//        );
-//      });
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//        appBar: AppBar(
-//          title: Text("Spacer and alignment"),
-//        ),
-//        body: Container(
-//            padding: EdgeInsets.all(8.0),
-//            child: Column(
-//              children: <Widget>[
-//                SizedBox(
-//                  height: 16.0,
-//                ),
-//                Text(
-//                    "Items are evenly distributed in the line with equal space around them (space around)"),
-//                SizedBox(
-//                  height: 8.0,
-//                ),
-//                Row(
-//                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                    children: _sampleWidget()),
-//                SizedBox(
-//                  height: 16.0,
-//                ),
-//                Text(
-//                    "Items Spread across the screen however keeps equal space in  between (space between)"),
-//                SizedBox(
-//                  height: 8.0,
-//                ),
-//                Row(
-//                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                    children: _sampleWidget()),
-//                SizedBox(
-//                  height: 16.0,
-//                ),
-//                Text(
-//                    "items are distributed so that the spacing between any two adjacent alignment subjects, before the first alignment subject, and after the last alignment subject is the same (Space evenly)"),
-//                SizedBox(
-//                  height: 8.0,
-//                ),
-//                Row(
-//                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                    children: _sampleWidget()),
-//              ],
-//            )));
-//  }
-//}
+class RowColumnAlignment extends StatelessWidget {
+  List colors = [Colors.teal, Colors.red, Colors.blue];
+
+  _sampleWidget() => List<Widget>.generate(colors.length, (index) {
+        return new Container(
+          color: colors[index],
+          height: 50,
+          width: 50,
+        );
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Spacer and alignment"),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                    "Items are evenly distributed in the line with equal space around them (space around)"),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: _sampleWidget()),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                    "Items Spread across the screen however keeps equal space in  between (space between)"),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: _sampleWidget()),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Text(
+                    "items are distributed so that the spacing between any two adjacent alignment subjects, before the first alignment subject, and after the last alignment subject is the same (Space evenly)"),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: _sampleWidget()),
+              ],
+            )));
+  }
+}
